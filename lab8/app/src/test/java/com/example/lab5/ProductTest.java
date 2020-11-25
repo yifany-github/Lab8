21004d68169d393b0188aab3e3832c5d6c854164
Yifan Yang <yifanyang119@gmail.com>
2020-11-24 21:20:35 -0400

    Create ProductTest.java

---
+++ lab8/app/src/test/java/com/example/lab5/ProductTest.java
@@ -0,0 +1,25 @@
+package com.example.lab5;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ProductTest {
+
+    @Test
+    public void checkProductName(){
+        Product aProduct = new Product("1","DELL MONITOR",180);
+        assertEquals("Check the name of the product","DELL MONITOR", aProduct.getProductName());
+    }
+
+    @Test
+    public void checkProductId(){
+        Product aProduct = new Product("1","DELL MONITOR",180);
+        assertEquals("Check the id of the product","1", aProduct.getId());
+    }
+
+    @Test
+    public void checkProductPrice(){
+        Product aProduct = new Product("1","DELL MONITOR",180);
+        assertEquals("Check the price of the product","0", aProduct.getPrice());
+    }
+}
